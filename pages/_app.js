0f2222cf41c09af51d445bf88029ec318d23d8e1
tianyumvp <tj.tianyujiang@gmail.com>
2025-11-30 17:34:44 +0800

    Add Google site verification meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 // pages/_app.js
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="R6UhVa46Ph2zXbuw6FYpSAeaFWjulAvFBiNdK5S50dM"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
